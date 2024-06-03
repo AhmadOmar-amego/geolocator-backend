9f50c41cb55daeb81a1440ae95ed1b8a5ddfd304
Ahmad Omar <ahmadomar.amego@gmail.com>
2024-06-04 00:12:10 +0300

    add datasbase module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LocationsModule } from './locations/locations.module';
+import { DatabaseModule } from './database/database.module';
+import { LocationModule } from './location/location.module';
 
 @Module({
-  imports: [LocationsModule],
+  imports: [LocationsModule, DatabaseModule, LocationModule],
   controllers: [AppController],
   providers: [AppService],
 })
