bd416ab0482c0727e98b793519e9b1d065814352
Ahmad Omar <ahmadomar.amego@gmail.com>
2024-06-04 00:15:13 +0300

    remove not wanted dto and entities

--- src/location-creation-logic/dto/update-location-creation-logic.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateLocationCreationLogicDto } from './create-location-creation-logic.dto';
-
-export class UpdateLocationCreationLogicDto extends PartialType(CreateLocationCreationLogicDto) {}
